88a7d6ffdb6c52a7aacc55513a0726b4621f0c50
Lay <karomahlailatul@gmail.com>
2024-03-10 09:22:56 +0700

    test 01

---
+++ dart_project/bin/01-string.dart
@@ -0,0 +1,4 @@
+void main() {
+  String data = 'halo dunia, selamat hari minggu';
+  print(data);
+}
---
+++ dart_project/bin/latihan-1_01.dart
@@ -0,0 +1,5 @@
+void main() {
+  String longName = "Lailatul Karomah";
+  String nama = "Hello Nama Saya :";
+  print(nama + longName);
+}
